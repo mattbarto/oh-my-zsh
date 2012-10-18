76e450575a7ed7a504d50901a1b5ab974fe7467d
Matt Barto <matt@milo.com>
2012-10-18 16:02:17 -0700

    chmod command

--- plugins/python/python.plugin.zsh
+++ plugins/python/python.plugin.zsh
@@ -3,3 +3,5 @@ alias pyfind='find . -name "*.py"'
 
 # Remove python compiled byte-code
 alias pyclean='find . -type f -name "*.py[co]" -exec rm -f \{\} \;'
+
+alias pynonexe='find . -type f -name "*.py" -exec chmod -x \{\} \;'
