990bbbfcf1e0390c6ede6d5a10f2e803b0c13608
Matt Barto <matt@supermassive.io>
2014-08-07 16:38:28 -0700

    Adding some aliased

--- lib/aliases.zsh
+++ lib/aliases.zsh
@@ -33,4 +33,17 @@ alias la='ls -lAh'
 alias sl=ls # often screw this up
 
 alias afind='ack-grep -il'
+alias subl='/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl'
+
+# git
+alias gl='git pull'
+alias gp='git push'
+alias gd='git diff'
+alias gc='git commit'
+alias gca='git commit -a'
+alias gco='git checkout'
+alias gb='git branch'
+alias gs='git status'
+alias grm="git status | grep deleted | awk '{print \$3}' | xargs git rm"
+alias changelog='git log `git log -1 --format=%H -- CHANGELOG*`..; cat CHANGELOG*'
 
